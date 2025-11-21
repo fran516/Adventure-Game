78708d33528afeb66032393b6e394a9c629a13b8
fran516 <rzq512@york.ac.uk>
2025-11-21 14:10:14 +0000

    21/11/25

---
+++ Assets/CollectAmmo.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CollectAmmo : MonoBehaviour
+{
+    public int ammo;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ammo > 0) {
+            ammo--;
+
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit result;
+            Physics.Raycast(ray, out result);
+            GameObject g = result.collider.gameObject;
+
+            if (g.name = "Target") {
+                Animation a = g.transform.parent.GetComponent<Animation>();
+                a.Play("LowerBridge");
+            }
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "AmmoBox") {
+            ammo = 20;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
